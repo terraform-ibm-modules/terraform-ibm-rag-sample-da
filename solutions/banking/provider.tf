e31b22913dfdc9041eee5e93f2c0f837472e30db
brendankellyibm <brendank@ie.ibm.com>
2024-04-12 13:56:26 +0100

    fix: attempt to fix provider issues

--- solutions/banking/provider.tf
+++ solutions/banking/provider.tf
@@ -2,3 +2,13 @@ provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.toolchain_region
 }
+
+provider "restapi" {
+  uri                  = "https:"
+  write_returns_object = true
+  debug                = true
+  headers = {
+    Authorization = data.ibm_iam_auth_token.tokendata.iam_access_token
+    Content-Type  = "application/json"
+  }
+}
