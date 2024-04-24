a6a8653e02c60188e100715105d33330e990fa66
Rajat Agrawal <108140212+rajatagarwal-ibm@users.noreply.github.com>
2024-04-24 10:02:37 +0100

    fix: do not create more than 1 assistant projects (#30)

--- solutions/banking/watson-scripts/assistant-project-creation.sh
+++ solutions/banking/watson-scripts/assistant-project-creation.sh
@@ -4,10 +4,23 @@ set -e
 
 IAM_TOKEN="$1"
 WATSON_ASSISTANT_URL=$2
+project_name="gen-ai-rag-sample-app-assistant"
 
 token="$(echo "$IAM_TOKEN" | awk '{print $2}')"
 
-curl -X POST --location "$WATSON_ASSISTANT_URL/v2/assistants?version=2023-06-15" \
+ASSISTANT_ID=$(curl --retry 3 -fLsS -X GET --location "$WATSON_ASSISTANT_URL/v2/assistants?version=2023-06-15" \
     --header "Authorization: Bearer $token" \
     --header "Content-Type: application/json" \
-    --data "{\"name\":\"gen-ai-rag-sample-app-assistant\",\"language\":\"en\",\"description\":\"Generative AI sample app assistant\"}"
+    | jq -r '.assistants[] | select(.name == "'$project_name'") | .assistant_id ')
+
+if [[ -z "$ASSISTANT_ID" ]]; then
+  # if not ASSISTANT_ID is found then create a new assistant project
+  curl --retry 3 -fLsS -X POST --location "$WATSON_ASSISTANT_URL/v2/assistants?version=2023-06-15" \
+      --header "Authorization: Bearer $token" \
+      --header "Content-Type: application/json" \
+      --data "{\"name\":\"$project_name\",\"language\":\"en\",\"description\":\"Generative AI sample app assistant\"}"
+else
+  # If ASSISTANT_ID exists in a project then do not create another project.
+  echo "$project_name project already exists."
+  exit 0
+fi
