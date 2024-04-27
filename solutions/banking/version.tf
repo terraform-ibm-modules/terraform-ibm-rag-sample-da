49bbb8785ff0e9b41abd601059c308c19ad73d83
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-04-27 06:09:59 +0100

    fix(deps): update required_provider to latest for deployable modules / architectures (#36)

--- solutions/banking/version.tf
+++ solutions/banking/version.tf
@@ -6,15 +6,15 @@ terraform {
     }
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.53.0"
+      version = ">= 1.64.2"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.1"
+      version = ">= 3.2.2"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.5.1"
+      version = ">= 3.6.1"
     }
     restapi = {
       source  = "Mastercard/restapi"
