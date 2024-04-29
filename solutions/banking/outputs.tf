e65065958d5df8fc7af9c899528b086c8cb99f68
Rajat Agrawal <rajatatibm@gmail.com>
2024-04-29 11:08:17 +0100

    fix: outputs and ci pipeline flag

--- solutions/banking/outputs.tf
+++ solutions/banking/outputs.tf
@@ -1,19 +1,19 @@
-output "project_id" {
+output "watsonx_project_id" {
   description = "ID of the created WatsonX project."
   value       = module.configure_project.project_id
 }
 
-output "project_url" {
+output "watsonx_project_url" {
   description = "WatsonX project ID URL."
   value       = "https://dataplatform.cloud.ibm.com/projects/${module.configure_project.project_id}"
 }
 
-output "watsonx_assistant_url" {
+output "watsonx_assistant_api_url" {
   description = "WatsonX Assistant URL."
   value       = local.watsonx_assistant_url
 }
 
-output "watsonx_discovery_url" {
+output "watsonx_discovery_api_url" {
   description = "WatsonX Discovery URL."
   value       = local.watsonx_discovery_url
 }
@@ -23,12 +23,12 @@ output "cos_instance_crn" {
   value       = module.cos.cos_instance_crn
 }
 
-output "assistant_integration_id" {
+output "watsonx_assistant_integration_id" {
   description = "WatsonX assistant integration ID."
   value       = data.external.assistant_get_integration_id.result.assistant_integration_id
 }
 
-output "discovery_project_id" {
+output "watsonx_discovery_project_id" {
   description = "WatsonX Discovery Project ID."
   value       = data.external.discovery_project_id.result.discovery_project_id
 }
