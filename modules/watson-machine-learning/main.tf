76282083dcd6dc917cb81fb4176cbfc5b8c2520f
Brendan Kelly <133764206+brendankellyibm@users.noreply.github.com>
2024-08-02 20:10:46 +0100

    feat: add storage delegation from SaaS DA for WatsonX project (#129)

--- modules/watson-machine-learning/main.tf
+++ modules/watson-machine-learning/main.tf
@@ -10,11 +10,26 @@ module "cos" {
   cos_plan          = "standard"
 }
 
+module "storage_delegation" {
+  providers = {
+    ibm                           = ibm
+    ibm.deployer                  = ibm
+    restapi.restapi_watsonx_admin = restapi.restapi_watsonx_admin
+  }
+  source               = "git::https://github.com/terraform-ibm-modules/terraform-ibm-watsonx-saas-da.git//storage_delegation?ref=v1.4.0"
+  count                = var.watsonx_project_delegated ? 1 : 0
+  cos_kms_crn          = var.cos_kms_crn
+  cos_kms_key_crn      = var.cos_kms_key_crn
+  cos_kms_new_key_name = var.cos_kms_new_key_name
+  cos_kms_ring_id      = var.cos_kms_ring_id
+  cos_guid             = module.cos.cos_instance_guid
+}
+
 ## Use code from Watson SaaS directly to avoid "legacy module" issues
 ## Note: passing a non-null delegated storage attribute may result in API errors
 
-
 resource "restapi_object" "configure_project" {
+  depends_on     = [module.storage_delegation]
   provider       = restapi.restapi_watsonx_admin
   path           = local.dataplatform_api
   read_path      = "${local.dataplatform_api}{id}"
