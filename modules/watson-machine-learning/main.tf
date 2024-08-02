ea1aab9208d473cc9a53008e95e8f3a2f10ec52c
brendankellyibm <brendank@ie.ibm.com>
2024-08-02 19:10:24 +0100

    fix: storage delegation should only occur if cos_kms_crn is not empty

--- modules/watson-machine-learning/main.tf
+++ modules/watson-machine-learning/main.tf
@@ -17,7 +17,7 @@ module "storage_delegation" {
     restapi.restapi_watsonx_admin = restapi.restapi_watsonx_admin
   }
   source               = "git::https://github.com/terraform-ibm-modules/terraform-ibm-watsonx-saas-da.git//storage_delegation?ref=v1.4.0"
-  count                = var.watsonx_project_delegated ? 0 : 1
+  count                = var.watsonx_project_delegated ? 1 : 0
   cos_kms_crn          = var.cos_kms_crn
   cos_kms_key_crn      = var.cos_kms_key_crn
   cos_kms_new_key_name = var.cos_kms_new_key_name
