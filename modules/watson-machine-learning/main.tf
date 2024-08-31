be349296816d5238a322e50a19e0f11d46b82e52
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-08-31 06:47:49 +0000

    fix(deps): update terraform terraform-ibm-modules/cos/ibm to v8.11.2

--- modules/watson-machine-learning/main.tf
+++ modules/watson-machine-learning/main.tf
@@ -4,7 +4,7 @@ module "cos" {
     ibm = ibm.ibm_resources
   }
   source            = "terraform-ibm-modules/cos/ibm//modules/fscloud"
-  version           = "8.10.7"
+  version           = "8.11.2"
   resource_group_id = var.resource_group_id
   cos_instance_name = var.cos_instance_name
   cos_plan          = "standard"
