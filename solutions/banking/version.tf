a1273c1c573916ad17faae1424b858cba64d6681
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-09-21 03:55:29 +0100

    fix(deps): update terraform elasticstack to latest for deployable architectures (#174)

--- solutions/banking/version.tf
+++ solutions/banking/version.tf
@@ -22,7 +22,7 @@ terraform {
     }
     elasticstack = {
       source  = "elastic/elasticstack"
-      version = ">= 0.11.6"
+      version = ">= 0.11.7"
     }
   }
   required_version = ">= 1.3.0"
