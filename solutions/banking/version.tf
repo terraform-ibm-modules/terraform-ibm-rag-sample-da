117016898d091ab6cdc23bb0f458555939750e03
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-09-27 10:47:33 +0100

    fix: lock into exact provider versions for DA (#182)

--- solutions/banking/version.tf
+++ solutions/banking/version.tf
@@ -1,32 +1,33 @@
 terraform {
+  # Lock DA into an exact provider version - renovate automation will keep it updated
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.69.2"
+      version = "1.69.2"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.3"
+      version = "3.2.3"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.6.3"
+      version = "3.6.3"
     }
     restapi = {
       source  = "Mastercard/restapi"
-      version = ">= 1.20.0"
+      version = "1.20.0"
     }
     shell = {
       source  = "scottwinkler/shell"
-      version = ">= 1.7.10"
+      version = "1.7.10"
     }
     elasticstack = {
       source  = "elastic/elasticstack"
-      version = ">= 0.11.7"
+      version = "0.11.7"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.32.0"
+      version = "2.32.0"
     }
   }
   required_version = ">= 1.3.0"
