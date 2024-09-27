75564aa729d36b7209862a3f4319c87a48eddcf4
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-09-27 10:33:33 +0100

    lock into exact provider version

--- solutions/banking/version.tf
+++ solutions/banking/version.tf
@@ -2,31 +2,31 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.69.2"
+      version = "1.69.2"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.3"
+      version = "3.2.3"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.6.3"
+      version = "3.6.3"
     }
     restapi = {
       source  = "Mastercard/restapi"
-      version = ">= 1.20.0"
+      version = "1.20.0"
     }
     shell = {
       source  = "scottwinkler/shell"
-      version = ">= 1.7.10"
+      version = "1.7.10"
     }
     elasticstack = {
       source  = "elastic/elasticstack"
-      version = ">= 0.11.7"
+      version = "0.11.7"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.32.0"
+      version = "2.32.0"
     }
   }
   required_version = ">= 1.3.0"
