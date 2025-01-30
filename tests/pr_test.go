75d8699ca18665e2ddefdf5c26251ecae22271d7
Vipin Kumar <vipinkumar@Vipins-MacBook-Pro.local>
2025-01-30 14:36:34 +0530

    SKIP UPGRADE TEST

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -121,7 +121,7 @@ func TestRunBankingSolutions(t *testing.T) {
 
 		options.TerraformVars["cluster_name"] = terraform.Output(t, existingTerraformOptions, "cluster_name")
 
-		output, err := options.RunTest()
+		output, err := options.RunTestConsistency()
 		assert.Nil(t, err, "This should not have errored")
 		assert.NotNil(t, output, "Expected some output")
 	}
