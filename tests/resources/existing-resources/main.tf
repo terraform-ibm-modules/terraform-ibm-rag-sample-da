865845a2ff48fb2b1c3afe902425cd264e9257e8
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-01 16:33:31 +0000

    fix(deps): update terraform-module

--- tests/resources/existing-resources/main.tf
+++ tests/resources/existing-resources/main.tf
@@ -165,7 +165,7 @@ module "ocp_base" {
 
   count                                = var.create_ocp_cluster ? 1 : 0
   source                               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                              = "3.35.10"
+  version                              = "3.41.3"
   resource_group_id                    = module.resource_group.resource_group_id
   region                               = var.region
   tags                                 = []
