2738ec95d9dd1803476dcdabc8f987ea7d012a66
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-11-03 20:22:57 +0000

    fix(deps): update dependencies (#325)
    * fix(deps): update dependencies
    
    * fix
    
    * SKIP UPGRADE TEST
    
    ---------
    
    Co-authored-by: ocofaigh <ocofaigh@ie.ibm.com>

--- tests/resources/existing-resources/main.tf
+++ tests/resources/existing-resources/main.tf
@@ -17,7 +17,7 @@ locals {
       pool_name        = "default" # ibm_container_vpc_cluster automatically names default pool "default" (See https://github.com/IBM-Cloud/terraform-provider-ibm/issues/2849)
       machine_type     = "bx2.4x16"
       workers_per_zone = 2 # minimum of 2 is allowed when using single zone
-      operating_system = "REDHAT_8_64"
+      operating_system = "RHEL_9_64"
     }
   ]
 }
@@ -28,7 +28,7 @@ locals {
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.6"
+  version = "1.4.0"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -163,20 +163,19 @@ resource "ibm_is_subnet" "subnet_zone_1" {
 
 module "ocp_base" {
 
-  count                                = var.create_ocp_cluster ? 1 : 0
-  source                               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                              = "3.35.10"
-  resource_group_id                    = module.resource_group.resource_group_id
-  region                               = var.region
-  tags                                 = []
-  cluster_name                         = var.prefix
-  force_delete_storage                 = true
-  vpc_id                               = ibm_is_vpc.vpc[0].id
-  vpc_subnets                          = local.cluster_vpc_subnets
-  ocp_version                          = null
-  worker_pools                         = local.worker_pools
-  access_tags                          = []
-  ocp_entitlement                      = null
-  disable_outbound_traffic_protection  = true # set as True to enable outbound traffic; required for accessing Operator Hub in the OpenShift console.
-  import_default_worker_pool_on_create = false
+  count                               = var.create_ocp_cluster ? 1 : 0
+  source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
+  version                             = "3.70.0"
+  resource_group_id                   = module.resource_group.resource_group_id
+  region                              = var.region
+  tags                                = []
+  cluster_name                        = var.prefix
+  force_delete_storage                = true
+  vpc_id                              = ibm_is_vpc.vpc[0].id
+  vpc_subnets                         = local.cluster_vpc_subnets
+  ocp_version                         = null
+  worker_pools                        = local.worker_pools
+  access_tags                         = []
+  ocp_entitlement                     = null
+  disable_outbound_traffic_protection = true # set as True to enable outbound traffic; required for accessing Operator Hub in the OpenShift console.
 }
