f56e559610305637e1c9caf030fab941a2654808
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2026-02-11 19:53:25 +0530

    test: uncommented regions as the issue is now fixed

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -4,12 +4,13 @@ package test
 import (
 	"bytes"
 	"fmt"
-	"github.com/google/uuid"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
 	"testing"
+
+	"github.com/google/uuid"
 
 	"github.com/IBM/go-sdk-core/v5/core"
 	"github.com/gruntwork-io/terratest/modules/files"
@@ -28,11 +29,9 @@ const bankingSolutionsDir = "solutions/banking"
 
 // watsonx.ai supported regions
 var validRegions = []string{
-	// Temporarily commenting out the regions as a workaround. Currently, tests are only passing in us-south and eu-de.
-	// For more details, see issue: https://github.com/terraform-ibm-modules/terraform-ibm-rag-sample-da/issues/345
-	// "au-syd",
-	// "jp-tok",
-	// "eu-gb",
+	"au-syd",
+	"jp-tok",
+	"eu-gb",
 	"eu-de",
 	"us-south",
 }
