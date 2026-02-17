e291ebe5ba9eb641806ec7e6065798c11dc051a0
Rajat Agrawal <rajatatibm@gmail.com>
2026-02-17 12:04:09 +0000

    add sm in tests

--- tests/resources/existing-resources/outputs.tf
+++ tests/resources/existing-resources/outputs.tf
@@ -64,11 +64,11 @@ output "region" {
 }
 
 output "secrets_manager_guid" {
-  value = module.secrets_manager.secrets_manager_guid
+  value       = module.secrets_manager.secrets_manager_guid
   description = "GUID of Secrets Manager instance"
 }
 
 output "secrets_manager_region" {
-  value = module.secrets_manager.secrets_manager_region
+  value       = module.secrets_manager.secrets_manager_region
   description = "Region of the Secrets Manager instance"
 }
