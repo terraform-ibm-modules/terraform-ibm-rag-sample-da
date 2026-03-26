daf62f2ad1873ad48a4f909d980501fa315e7130
Akash Kumar <akash.kumar@ibm.com>
2026-03-27 01:29:42 +0530

    updated sm gp Deafult

--- solutions/banking/outputs.tf
+++ solutions/banking/outputs.tf
@@ -57,13 +57,3 @@ output "sample_app_public_url" {
   description = "URL of the public route of the sample app deployed on ROKS cluster"
   value       = var.cluster_name != null && var.provision_public_ingress ? "https://gen-ai-rag-sample-app-tls-dev.${module.cluster_ingress[0].cluster_workload_ingress_subdomain}" : null
 }
-
-output "secret_group_name" {
-  description = "Name of the secret group that will store the generated secrets."
-  value       = local.secret_group_name
-}
-
-output "secret_group_id" {
-  description = "The ID of the created secret group."
-  value       = local.secret_group_id
-}
