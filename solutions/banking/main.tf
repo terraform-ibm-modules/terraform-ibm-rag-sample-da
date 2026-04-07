61879d93ebc7ce5e45a2a11c1fdf5379ec19952e
Akash Kumar <akash.kumar@ibm.com>
2026-04-07 14:44:30 +0530

    testing sm

--- solutions/banking/main.tf
+++ solutions/banking/main.tf
@@ -86,7 +86,7 @@ module "gpg_signing_key" {
   sm_name             = data.ibm_resource_instance.secrets_manager_name[0].name
   sm_endpoint_type    = var.secrets_manager_endpoint_type
   sm_exists           = true
-  create_secret_group = false
+  create_secret_group = true
   create_signing_key  = true
 }
 
